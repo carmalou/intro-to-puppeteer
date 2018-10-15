d0461a5f0756b4e0d20b738ae41fdcd957f8e7c3
carmalou <carmenbourlonlong@gmail.com>
2018-10-14 21:59:50 -0500

    pdf examples

---
+++ samples/generate-pdf-with-markup.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+const html = "<h1>Hello OKC.js! Isn't Puppeteer magical?";
+
+(async () => {
+    var fileName = path.join(__dirname, './pdfs/') + 'sample-pdf-' + Date.now() + '.pdf';
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html);
+    await page.pdf({
+      path: fileName, 
+      format: 'Letter',
+      margin: {
+        top: '1in',
+        bottom: '1in',
+        left: '1in',
+        right: '1in'
+      }
+    });
+  
+    await browser.close();
+  })();
